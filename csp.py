d35af93c6728c80f16d274c71bd9add5daec7304
n0r2 <leave.orz@gmail.com>
2017-10-22 04:16:56 +0900

    リアルタイムで出力が見れるようにしました

--- csp.py
+++ csp.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cv2
+import sys
+
 
 HEIGHT = 70
 WIDTH = 1252  # 魔法の数字
@@ -7,8 +9,10 @@ pts1 = np.float32([[42, 0], [0, HEIGHT], [1210, 0], [WIDTH, HEIGHT]])
 pts2 = np.float32([[0, 0], [0, HEIGHT], [WIDTH, 0], [WIDTH, HEIGHT]])
 M = cv2.getPerspectiveTransform(pts1, pts2)
 
-cap = cv2.VideoCapture('video.mp4')  # FullHD 60fps
+cap = cv2.VideoCapture(sys.argv[1])  # FullHD 60fps
 cnt = 0
+out = np.zeros((700, 125, 3), np.uint8)
+
 while(cap.isOpened()):
     ret, frame = cap.read()
     if not ret:
@@ -19,11 +23,16 @@ while(cap.isOpened()):
     print(cnt)
 
     pers = cv2.warpPerspective(cut, M, (WIDTH, HEIGHT))
-    cv2.imshow('frame', pers)
+    cv2.imshow('trimm', cut)
+    cv2.imshow('pers', pers)
+    resize_pers = cv2.resize(pers, (125, 7))
+    out[(99 - (cnt % 100)) * 7:(99 - (cnt % 100)) * 7 + 7, :] = resize_pers
+    cv2.imshow('out', out)
     cv2.imwrite('png/%05d.png' % cnt, pers)
     cnt += 1
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 
 cap.release()
 cv2.destroyAllWindows()
